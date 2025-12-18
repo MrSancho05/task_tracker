3d56e35c451b29d9a82f583b8529a703cd848b41
MrSancho05 <bahrombekovsanjarbek@gmail.com>
2025-12-18 20:23:57 +0500

    add method in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tasks import load_tasks, save_tasks
 from datetime import datetime
+import sys
 
 def main():
     tasks = load_tasks()
@@ -7,19 +8,33 @@ def main():
     print('Todos:', tasks)
     print('='*50)
 
-    # new task
-    # now = datetime.now().isoformat(timespec='second')
-    now = datetime.now().isoformat(timespec='seconds')
-    new_task = {
-        'id': 1 if not tasks else max(task['id'] for task in tasks) + 1,
-        'description': 'Test new task',
-        'status': 'todo',
-        'createdAt': now,
-        'updatedAt': now
-    }
-    tasks.append(new_task)
-    save_tasks(tasks)
-    print('New task added & saved!')
+    args = sys.argv
+
+    if len(args) < 2:
+        print('python main.py dan keyin: "add" kiriting')
+        return
+    
+    command = args[1]
+
+    if command == 'add':
+        if len(args) < 3:
+            print('Task matnini kiriting')
+            return
+
+        desctiption = args[2]
+        # new task
+        now = datetime.now().isoformat(timespec='seconds')
+        new_task = {
+                'id': 1 if not tasks else max(task['id'] for task in tasks) + 1,
+                'description': desctiption,
+                'status': 'todo',
+                'createdAt': now,
+                'updatedAt': now
+            }
+        tasks.append(new_task)
+        save_tasks(tasks)
+        print(f'Task added: {new_task} {desctiption}')
+
 
 
 
